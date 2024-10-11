893343edf3d85e263894e1a5b56e6f0ef2c30348
maroreigns <138331891+maroreigns@users.noreply.github.com>
2024-10-11 14:12:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
-
+function toggleImage(button) {
+    const card = button.closest('.card');
+    const img1 = card.querySelector('.img1');
+    const img2 = card.querySelector('.img2');
+  
+    if (img1.classList.contains('d-none')) {
+      img1.classList.remove('d-none');
+      img2.classList.add('d-none');
+      button.textContent = 'View Image';
+    } else {
+      img1.classList.add('d-none');
+      img2.classList.remove('d-none');
+      button.textContent = 'Back to Image';
+    }
+  }
+  
 
   
 
