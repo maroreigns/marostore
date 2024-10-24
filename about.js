98eddb09a16d9fb9e09e4d56670407f494cbf9b2
maroreigns <138331891+maroreigns@users.noreply.github.com>
2024-10-23 17:25:34 -0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,30 @@
+//  // Get the navbar element
+const navbar = document.querySelector('.navbar');
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    navbar.classList.remove('navbar-scrolled');
+  }
+});
+
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// scroll to top function
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollTopBtn.style.opacity = "1";  // Fully visible
+    scrollTopBtn.style.pointerEvents = "auto";  // Enable click
+  } else {
+    scrollTopBtn.style.opacity = "1";  // Partially visible
+    scrollTopBtn.style.pointerEvents = "none";  // Prevent click when not needed
+  }
+};
+
+// Scroll to the top when the button is clicked
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
